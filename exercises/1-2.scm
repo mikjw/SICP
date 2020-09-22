fce03d9bf2c31cb2726c3d258ff81574a7efd266
Mike <mjwalshe@gmail.com>
2020-09-22 22:09:14 +0100

    First three exercises

---
+++ exercises/1-2.scm
@@ -0,0 +1,9 @@
+; Exercise 1.2.  Translate the following expression into prefix form
+; image: https://mitpress.mit.edu/sites/default/files/sicp/full-text/book/ch1-Z-G-3.gif
+
+(/ (+ 5
+        4
+        (- 2 (- 3 (+ 6 (/ 4 5)))))
+    (* 3
+        (- 6 2)
+        (- 2 7)))
---
+++ exercises/1-3.scm
@@ -0,0 +1,14 @@
+; Exercise 1.3.  Define a procedure that takes three 
+; numbers as arguments and returns the sum of the 
+; squares of the two larger numbers.
+
+(define (square x) 
+    (* x x))
+
+(define (sum-of-squares x y) 
+    (+ (square x) (square y)))
+
+(define (sum-larger-squares x y z)
+    (cond   ((and (>= x z) (>= y z)) (sum-of-squares x y))
+            ((and (>= x y) (>= z y)) (sum-of-squares x z))
+            ((and (>= y x) (>= z x)) (sum-of-squares y z))))  
